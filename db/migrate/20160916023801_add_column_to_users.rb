712f18b8c461fa378bc1501aeca90387b07a7e7e
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-09-16 12:03:09 +0900

    add gem 'devise' and create user model, users table, users_controller

---
+++ db/migrate/20160916023801_add_column_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :member, :string
+    add_column :users, :profile, :text
+    add_column :users, :works, :text
+  end
+end
