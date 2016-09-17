1daf956bf25221474fa01d04c19be57364eae3f4
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-09-17 18:24:53 +0900

    add user edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+  def edit
+  end
+
+  def update
+    current_user.update(update_params)
+    redirect_to :root
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:name, :member, :profile, :works)
+  end
 end
