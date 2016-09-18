8c3753edc2f7488a5ebabe09fe63e3be02f7c49a
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-09-18 17:55:02 +0900

    fix configure_permitted_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :member, :profile, :works])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :member, :profile, :works])
   end
 end
