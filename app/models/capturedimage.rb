80c08c3623038468c3e1e993fb143946257cc267
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-09-19 18:03:11 +0900

    Add relation with prototype and captured images

--- app/models/capturedimage.rb
+++ app/models/capturedimage.rb
@@ -1,2 +1,5 @@
 class Capturedimage < ActiveRecord::Base
+  belongs_to :prototype
+  # carrierwaveとモデルの関連付け
+  mount_uploader :picture, PictureUploader
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,3 +1,5 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
+  has_many :capturedimages
+  accepts_nested_attributes_for :capturedimages
 end
