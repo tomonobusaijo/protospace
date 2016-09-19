32a8fe49b81c8a838962d14c474b405147d5af08
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-09-19 17:06:25 +0900

    fix prototypes_controller

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,8 +3,20 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
   end
 
   def show
+  end
+
+  def create
+    @prototype = Prototype.create(create_params)
+    flash[:success] = "Prototype posting is completed."
+    redirect_to :root and return
+  end
+
+  private
+  def create_params
+    params.require(:prototype).permit(:title, :catchcopy, :concept).merge(user_id: current_user.id)
   end
 end
