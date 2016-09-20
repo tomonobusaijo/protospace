38282973c636c175bf1301002962aef847480e05
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-09-20 17:28:09 +0900

    add_index to captureimage

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919085753) do
+ActiveRecord::Schema.define(version: 20160920024948) do
 
   create_table "capturedimages", force: :cascade do |t|
     t.integer  "prototype_id", limit: 4
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20160919085753) do
     t.datetime "updated_at",               null: false
     t.string   "picture",      limit: 255
   end
+
+  add_index "capturedimages", ["role"], name: "index_capturedimages_on_role", using: :btree
 
   create_table "prototypes", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
