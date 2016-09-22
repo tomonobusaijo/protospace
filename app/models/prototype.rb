8f4108cbd8c7333d1c82b339ff271abfa3c8057a
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-09-22 12:34:14 +0900

    Add allow_destroy, reject_if option

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,5 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :capturedimages
-  accepts_nested_attributes_for :capturedimages
+  accepts_nested_attributes_for :capturedimages, allow_destroy: true, reject_if: proc { |attributes| attributes['picture'].blank? }
 end
