6448dc41236138b0ba47ae54dfc58594843f3934
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-09-22 15:23:10 +0900

    add argument in #show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def update
     current_user.update(update_params)
     flash[:success] = "Profile edit is completed."
