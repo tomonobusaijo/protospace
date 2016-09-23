770644195f619e88d464cd4d81c0b6cb3dbc9762
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-09-23 17:49:29 +0900

    Prototype Model / add dependent

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,5 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
-  has_many :capturedimages
+  has_many :capturedimages, dependent: :delete_all
   accepts_nested_attributes_for :capturedimages, allow_destroy: true, reject_if: proc { |attributes| attributes['picture'].blank? }
 end
