d2b6cacd631c4ee654937109fdd3af96553f9371
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-09-26 00:58:20 +0900

    model(like, prototype, user) "add association"

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,7 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :capturedimages, dependent: :delete_all
+  has_many :likes, dependent: :delete_all
   accepts_nested_attributes_for :capturedimages, allow_destroy: true, reject_if: proc { |attributes| attributes['picture'].blank? }
   validates :title, :catchcopy, :concept, presence: true
 end
