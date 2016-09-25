222826ff1f7e0dd8420977e064223b07b79ae832
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-09-26 01:01:58 +0900

    users_controller "fix flash message"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
   def update
     current_user.update(update_params)
-    flash[:success] = "Profile edit is completed."
+    flash[:success] = "Profile was successfully updated."
     redirect_to :root and return
   end
 
