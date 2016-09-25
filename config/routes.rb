6face3cf8a44b757ea7ccaceed2dc1d2bc800f08
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-09-25 17:03:40 +0900

    routing fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  resources :prototypes
+  resources :prototypes, only: [:edit, :show, :update]
   resources :users
 end
