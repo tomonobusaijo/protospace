7bd91e288b16a85d57993a7984531335b2378778
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-09-26 12:21:17 +0900

    add prototype and user model to "include Paginate"

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,4 +1,5 @@
 class Prototype < ActiveRecord::Base
+  include Paginate
   belongs_to :user
   has_many :capturedimages, dependent: :delete_all
   accepts_nested_attributes_for :capturedimages, allow_destroy: true, reject_if: proc { |attributes| attributes['picture'].blank? }
