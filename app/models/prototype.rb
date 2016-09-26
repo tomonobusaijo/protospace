1879a8d24101519605df07e035423035bcac80ea
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-09-26 21:33:55 +0900

    models/prototype.rb "add like_user"

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,4 +4,8 @@ class Prototype < ActiveRecord::Base
   has_many :likes, dependent: :delete_all
   accepts_nested_attributes_for :capturedimages, allow_destroy: true, reject_if: proc { |attributes| attributes['picture'].blank? }
   validates :title, :catchcopy, :concept, presence: true
+
+  def like_user(user)
+    likes.find_by(user_id: user)
+  end
 end
