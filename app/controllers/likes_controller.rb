95b89c63eac65c70232654d54113f206e0d409e6
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-09-27 14:46:06 +0900

    likes_controller "simplify destroy action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,8 +6,7 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    @like = current_user.likes.find_by(prototype_id: params[:prototype_id])
-    @like.destroy
+    current_user.likes.find_by(prototype_id: params[:prototype_id]).destroy
     @likes = Like.where(prototype_id: params[:prototype_id])
     @prototype = Prototype.find(params[:prototype_id])
   end
