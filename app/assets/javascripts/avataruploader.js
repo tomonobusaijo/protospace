7a29120b74a5accc32d18525aceba1d3a5dc8271
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-10-17 11:17:42 +0900

    add conditional expression, adapt turbolinks

--- app/assets/javascripts/avataruploader.js
+++ app/assets/javascripts/avataruploader.js
@@ -1,16 +1,18 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('turbolinks:load', function() {
   var uploadAvatar = document.getElementById('upload_avatar');
   var avatarImageId = document.getElementById('user_avatar');
-  uploadAvatar.addEventListener('change', function(e){
-    var input = uploadAvatar.files[0];
-    var reader = new FileReader();
-    reader.addEventListener('load', function(e) {
-      avatarImageId.src = reader.result;
+  if ( uploadAvatar != null ){
+    uploadAvatar.addEventListener('change', function(e){
+      var input = uploadAvatar.files[0];
+      var reader = new FileReader();
+      reader.addEventListener('load', function(e) {
+        avatarImageId.src = reader.result;
+      }, true);
+      reader.readAsDataURL(input);
     }, true);
-    reader.readAsDataURL(input);
-  }, true);
 
-  avatarImageId.addEventListener('load', function(){
-    this.style.display = 'block';
-  });
+    avatarImageId.addEventListener('load', function(){
+      this.style.display = 'block';
+    });
+  }
 });
