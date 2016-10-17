54e910eb8c23d08271082519bf5341cdb1df7f05
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-10-17 11:15:21 +0900

    add conditional expression, adapt turbolinks

--- app/assets/javascripts/protouploader.js
+++ app/assets/javascripts/protouploader.js
@@ -1,14 +1,16 @@
-window.addEventListener('DOMContentLoaded', function() {
-  var elements = document.querySelectorAll('input[type = file]');
-  for ( i = 0, len = elements.length; i < len; i ++ ){
-    elements[i].addEventListener('change', function(e){
-      var input = document.getElementById(e.target.id).files[0];
-      var imageId = e.target.nextElementSibling.id;
-      var reader = new FileReader();
-      reader.addEventListener('load', function(e) {
-        document.getElementById(imageId).src = reader.result;
+window.addEventListener('turbolinks:load', function() {
+  var protoImages = document.querySelectorAll('.proto_uploader');
+  if ( protoImages != null ){
+    for ( i = 0, len = protoImages.length; i < len; i ++ ){
+      protoImages[i].addEventListener('change', function(e){
+        var input = document.getElementById(e.target.id).files[0];
+        var imageId = e.target.nextElementSibling.id;
+        var reader = new FileReader();
+        reader.addEventListener('load', function(e) {
+          document.getElementById(imageId).src = reader.result;
+        }, true);
+        reader.readAsDataURL(input);
       }, true);
-      reader.readAsDataURL(input);
-    }, true);
+    }
   }
 });
