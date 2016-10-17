6b35a599a3f7c581e1732a062e636409a28336e0
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-10-17 20:43:42 +0900

    create carrierwave.rb

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,18 @@
+if Rails.env.test?
+  CarrierWave.configure do |config|
+    config.storage = :file
+  end
+else
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_credentials = {
+        provider: 'AWS',
+        aws_access_key_id: ENV['ACCESS_KEY_ID'],
+        aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+        region: 'us-east-1'
+    }
+
+    config.fog_directory  = 'upload-test-hoge'
+    config.asset_host = 'https://s3.amazonaws.com/upload-test-hoge'
+  end
+end
