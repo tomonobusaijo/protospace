424ca485d064c65bf15ebde69ae1d19ba8d86416
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-10-18 21:02:31 +0900

    create test data

---
+++ spec/factories/caputuredimages.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :capturedimage do
+    name                    "test"
+    email                   "test@gmail.com"
+    password                "00000000"
+    password_confirmation   "00000000"
+  end
+
+end
---
+++ spec/factories/prototypes.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+
+  factory :prototype do
+    title       "test"
+    catchcopy   "test"
+    concept     "test"
+    like        "0"
+    comment     "0"
+  end
+
+end
