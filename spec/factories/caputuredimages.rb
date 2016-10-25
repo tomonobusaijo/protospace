e6aa2edde8de06291c6a26a75f5ee4207c9b3372
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-10-25 17:34:32 +0900

    ファクトリ修正・追加

--- spec/factories/caputuredimages.rb
+++ spec/factories/caputuredimages.rb
@@ -1,10 +1,22 @@
+include ActionDispatch::TestProcess
+
 FactoryGirl.define do
 
   factory :capturedimage do
-    name                    "test"
-    email                   "test@gmail.com"
-    password                "00000000"
-    password_confirmation   "00000000"
+    created_at  { Faker::Time.between(2.days.ago, 1.days.ago, :all) }
+    updated_at  { Faker::Time.between(1.days.ago, Time.now, :all) }
+    picture { fixture_file_upload("spec/fixtures/img/sample.jpg", "image/jpg") }
+
+    trait :main do
+      role   0
+    end
+
+    trait :sub do
+      role   1
+    end
+
+    factory :main_image, traits: [:main]
+    factory :sub_image, traits: [:sub]
   end
 
 end
--- spec/factories/prototypes.rb
+++ spec/factories/prototypes.rb
@@ -1,11 +1,48 @@
 FactoryGirl.define do
 
   factory :prototype do
-    title       "test"
-    catchcopy   "test"
-    concept     "test"
-    like        "0"
-    comment     "0"
+    title            { Faker::Name.name }
+    catchcopy        { Faker::Lorem.word }
+    concept          { Faker::Lorem.sentence }
+    created_at       { Faker::Time.between(2.days.ago, 1.days.ago, :all) }
+    updated_at       { Faker::Time.between(1.days.ago, Time.now, :all) }
+    user
+    main_image
+
+    trait :with_sub_images do
+      transient do   # テーブルにないカラムに対して使う(transient: つかの間の、一瞬の)
+        sub_images_count   { Faker::Number.between(1, 3) }
+      end
+
+      after(:build) do |prototype, evaluator|
+        build_list(:sub_image, evaluator.sub_images_count, prototype: prototype)
+      end
+
+      after(:create) do |prototype, evaluator|
+        create_list(:sub_image, evaluator.sub_images_count, prototype: prototype)
+      end
+    end
+
+    trait :with_likes do
+      transient do
+        likes_count      { Faker::Number.between(1, 5) }
+      end
+
+      after(:create) do |prototype, evaluator|
+        create_list(:like, evaluator.likes_count, prototype: prototype)
+      end
+    end
+
+    trait :with_comments do
+      transient do
+        comments_count   { Faker::Number.between(1, 5) }
+      end
+
+      after(:create) do |prototype, evaluator|
+        create_list(:comment, evaluator.comments_count, prototype: prototype)
+      end
+    end
+
   end
 
 end
